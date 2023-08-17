9a1b2db45fd937d43e54f2363e7cc259d8e98142
Tiago Bacciotti Moreira <baciotti@gmail.com>
2023-08-17 18:49:58 +0000

    ok

--- src/main/java/com/example/AppListas.java
+++ src/main/java/com/example/AppListas.java
@@ -8,6 +8,28 @@ public class AppListas {
         //Cliente[] vetorClientes = new Cliente[5];
         List<Cliente> listaClientes = new ArrayList<>();
 
+
+        boolean minhavariavel = true;
+
+        String nome = "Tiago";
+
+        int idade;
+        idade =45;
+
+        // cast conversão de tipo
+
+        char sexo = 'M';
+
+        double idade2;
+        idade2 = (double)idade;
+
+        
+
+        System.out.println("O nome é " + nome);
+
+
+
+
         var cliente1 = new Cliente();
         cliente1.setNome("Tiago");
 
